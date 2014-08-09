77eaea505afb6efe290f042324a10ca835eeaeb1
Stanislav Korotaev <superstals@gmail.com>
2014-08-09 19:28:00 +0400

    bullet speed is not set in gunner

--- gamejam2014/Assets/Scripts/Gunner.cs
+++ gamejam2014/Assets/Scripts/Gunner.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Gunner : MonoBehaviour {
 
     public Vector2 gunOffset = new Vector2(0f, 0f);
+    public float bulletSpeed = 0.2f;
 
     public float shotDelay = 0.05f;
     public float shotTimeElapsed;
@@ -40,6 +41,8 @@ public class Gunner : MonoBehaviour {
             GameObject bulletObject = (GameObject)(Instantiate(bulletPrefab, new Vector3(currentPosition.x + gunOffset.x, currentPosition.y + gunOffset.y, 0), transform.rotation));
             bulletObject.tag = this.gameObject.tag;
             bulletObject.layer = this.gameObject.layer;
+
+            bulletObject.GetComponent<BulletController>().speed = new Vector3(bulletSpeed, 0);
 
             //Game.Instance.getPlayer().removeHP(10);
         }
