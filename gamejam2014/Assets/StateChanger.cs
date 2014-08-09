2736973a4cdd3898bb38f24f9710c9c73e25ab29
Stanislav Korotaev <superstals@gmail.com>
2014-08-09 21:54:12 +0400

    refactor

--- gamejam2014/Assets/StateChanger.cs
+++ gamejam2014/Assets/StateChanger.cs
@@ -29,27 +29,34 @@ public class StateChanger : MonoBehaviour {
         if (Input.GetKeyDown(changeButton))
         {
             if(Game.Instance.getCurrentState() == GameState.Fly){
-                Game.Instance.setCurrentState( GameState.Walk );
-
-                mainCam.orthographicSize = closeSize;
-
-                Vector3 playerPosition = Game.Instance.getPlayerShip().transform.position;
-                mainCam.transform.position = new Vector3(playerPosition.x, playerPosition.y, -10);
-
-                shipOverlay.SetActive(false);
-
+                changeToWalk();
             }
 
             else if(Game.Instance.getCurrentState() == GameState.Walk){
-                Game.Instance.setCurrentState( GameState.Fly );
-                
-                mainCam.orthographicSize = farSize;
-                mainCam.transform.position = new Vector3(0, 0, -10);
-                shipOverlay.SetActive(true);
-
-
+                changeToFly();
             }
         }
+    }
+
+    void changeToWalk()
+    {
+        Game.Instance.setCurrentState( GameState.Walk );
+        
+        mainCam.orthographicSize = closeSize;
+        
+        Vector3 playerPosition = Game.Instance.getPlayerShip().transform.position;
+        mainCam.transform.position = new Vector3(playerPosition.x, playerPosition.y, -10);
+        
+        shipOverlay.SetActive(false);
+    }
+
+    void changeToFly()
+    {
+        Game.Instance.setCurrentState( GameState.Fly );
+        
+        mainCam.orthographicSize = farSize;
+        mainCam.transform.position = new Vector3(0, 0, -10);
+        shipOverlay.SetActive(true);
     }
 
     void updateLabel()
