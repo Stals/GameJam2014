dc0c9f6f2fd00873c7034a801676270ca16573c0
Stanislav Korotaev <superstals@gmail.com>
2014-08-09 23:01:34 +0400

    show hint when needed

---
+++ gamejam2014/Assets/EngineObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineObject : MonoBehaviour {
+
+    public GameObject hint;
+
+	// Use this for initialization
+	void Start () {
+        hint.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.layer == 12) // WalkingPlayer
+        {
+            hint.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll) {
+        if (coll.gameObject.layer == 12) // WalkingPlayer
+        {
+            hint.SetActive(false);
+        }
+    }
+}
