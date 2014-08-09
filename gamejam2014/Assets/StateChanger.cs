25ae704b96c194242f4284ea1e997492a80eb3b0
Stanislav Korotaev <superstals@gmail.com>
2014-08-09 21:23:49 +0400

    change states кривая версия

--- gamejam2014/Assets/StateChanger.cs
+++ gamejam2014/Assets/StateChanger.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class StateChanger : MonoBehaviour {
 
     public KeyCode changeButton;
+    public Camera mainCam;
+
+    public float closeSize;
+    public float farSize;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +22,22 @@ public class StateChanger : MonoBehaviour {
     void FixedUpdate(){
         if (Input.GetKey(changeButton))
         {
+            if(Game.Instance.getCurrentState() == GameState.Fly){
+                Game.Instance.setCurrentState( GameState.Walk );
+
+                mainCam.orthographicSize = closeSize;
+
+                Vector3 playerPosition = Game.Instance.getPlayerShip().transform.position;
+                mainCam.transform.position = new Vector3(playerPosition.x, playerPosition.y, -10);
+                // TODO -10 не забыть
+            }
+
+            else if(Game.Instance.getCurrentState() == GameState.Walk){
+                Game.Instance.setCurrentState( GameState.Fly );
+                
+                mainCam.orthographicSize = farSize;
+                mainCam.transform.position = new Vector3(0, 0, -10);
+            }
         }
     }
 }
