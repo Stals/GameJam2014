dcec181ac649cf2f6042b980e3a35f804edb5a67
Stanislav Korotaev <superstals@gmail.com>
2014-08-09 19:23:55 +0400

    enemy rotation

--- gamejam2014/Assets/EnemyController.cs
+++ gamejam2014/Assets/EnemyController.cs
@@ -29,16 +29,68 @@ public class EnemyController : MonoBehaviour {
 
     void FixedUpdate()
     {
-       
+        updateRotation();
+        updateGun();
+    }
 
+    void updateGun()
+    {
         if (timeElapsed > startShootingDelay)
         {
             gunner.shoot();
-
+            
         } else{ 
             timeElapsed += Time.deltaTime;
         }
     }
+
+    void updateRotation()
+    {
+
+        Quaternion rotation = Quaternion.LookRotation
+            (playerShip.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+
+
+        //transform.LookAt(playerShip.transform.position);
+
+
+        // TODO look - get rotation, rotate back?))
+        // mb this
+        //http://gamedev.stackexchange.com/questions/18513/algo-for-rotating-tower-towards-enemy-unity
+
+        /*Vector3 newForward = Vector3.Normalize(transform.position - playerShip.transform.position);
+        // calc the rotation so the avatar faces the target
+        Quaternion Rotation = GetRotation(Vector3.Forward, newForward, Vector3.Up);
+        //Cannon.Shoot(Position, Rotation, this);
+
+        transform.rotation = Rotation;*/
+
+    }
+
+    /*public static Quaternion GetRotation(Vector3 source, Vector3 dest, Vector3 up)
+    {
+        float dot = Vector3.Dot(source, dest);
+        
+        if (Math.Abs(dot - (-1.0f)) < 0.000001f)
+        {
+            // vector a and b point exactly in the opposite direction, 
+            // so it is a 180 degrees turn around the up-axis
+            return new Quaternion(up, MathHelper.ToRadians(180.0f));
+        }
+        if (Math.Abs(dot - (1.0f)) < 0.000001f)
+        {
+            // vector a and b point exactly in the same direction
+            // so we return the identity quaternion
+            return Quaternion.Identity;
+        }
+        
+        float rotAngle = (float)Math.Acos(dot);
+        Vector3 rotAxis = Vector3.Cross(source, dest);
+        rotAxis = Vector3.Normalize(rotAxis);
+        return Quaternion.CreateFromAxisAngle(rotAxis, rotAngle);
+    }*/
+
 
     void OnCollisionEnter2D(Collision2D coll) {
         if (coll.gameObject.tag != this.gameObject.tag)
