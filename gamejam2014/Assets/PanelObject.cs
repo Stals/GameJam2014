116217719ff436b6265199135a5e9d3444be6ec8
Stanislav Korotaev <superstals@gmail.com>
2014-08-10 00:35:14 +0400

    prefab update

--- gamejam2014/Assets/PanelObject.cs
+++ gamejam2014/Assets/PanelObject.cs
@@ -5,6 +5,20 @@ public class PanelObject : ShipObject {
 
     public StateChanger stateChanger;
   
+    /*void FixedUpdate()
+    {
+        if (Game.Instance.getCurrentState() != GameState.Walk){
+            return;    
+        }
+        
+        if (inside)
+        {
+            if(Input.GetKeyDown(KeyCode.Tab)){
+                performAction();
+            }
+        }
+    }*/
+
     public override void performAction()
     {
         stateChanger.changeToFly();
