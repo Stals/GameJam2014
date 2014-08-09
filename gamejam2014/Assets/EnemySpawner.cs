e2ba6b1f0f383c7b7398d4501d64606dd0628013
Stanislav Korotaev <superstals@gmail.com>
2014-08-09 19:49:30 +0400

    spawn new enemies

---
+++ gamejam2014/Assets/EnemySpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public List<GameObject> patternPrefabs;
+    public float waveDelay = 10;
+
+    public float timeElapsed;
+
+	// Use this for initialization
+	void Start () {
+        timeElapsed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= waveDelay)
+        {
+            timeElapsed = 0;
+            spawnWave();
+        }
+    }
+
+    void spawnWave()
+    {
+        int patternID = Random.Range(0, patternPrefabs.Count);
+        GameObject prefab = patternPrefabs[patternID];
+
+        Vector3 spawnPosition = getPatternStartPosition();
+
+        Instantiate(prefab, new Vector3(spawnPosition.x, spawnPosition.y, 0), prefab.transform.rotation);
+    }
+
+    Vector3 getPatternStartPosition()
+    {
+        return new Vector3(4, 0, 0);
+    }
+}
