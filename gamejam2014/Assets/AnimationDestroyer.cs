7af48dffaccf1bf4e1ca3e8afb7c5f6ba6428981
Stanislav Korotaev <superstals@gmail.com>
2014-08-10 10:22:04 +0400

    explosion prefab setup

---
+++ gamejam2014/Assets/AnimationDestroyer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationDestroyer : MonoBehaviour {
+
+    public float timeBeforeDestroy = 3f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(destr()); //this will run your timer
+
+	}
+
+    
+    IEnumerator destr() {
+        yield return new WaitForSeconds(timeBeforeDestroy); //this will wait 5 seconds
+        Destroy(gameObject);
+    }
+}
